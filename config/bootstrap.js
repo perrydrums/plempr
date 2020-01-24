552d94280a5e26688ea2dc83cd6a26e08fa563da
Perry Janssen <perry@burst-digital.com>
2020-01-24 10:38:42 +0100

    fix: dont add messages on boot

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,42 +9,6 @@
  * https://sailsjs.com/config/bootstrap
  */
 
-module.exports.bootstrap = async function(cb) {
-
+module.exports.bootstrap = function() {
   sails.config.appName = 'Sails Chat App';
-
-  // Generate Chat Messages
-  try {
-    let messageCount = ChatMessage.count();
-    if (messageCount > 0){
-      return;
-    }
-
-    let users = await User.find();
-    if (users.length >= 3) {
-      console.log('Generating messages...');
-
-      await ChatMessage.create({
-        message: 'Hey Everyone! Welcome to the community!',
-        createdBy: users[1].id
-      });
-
-      await ChatMessage.create({
-        message: 'How\'s it going?',
-        createdBy: users[2].id
-      });
-
-      await ChatMessage.create({
-        message: 'Super excited!',
-        createdBy: users[0].id
-      });
-
-    } else {
-      console.log('skipping message generation');
-    }
-  } catch(err){
-    console.error(err);
-  }
-
-  cb();
 };
