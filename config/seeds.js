d79a1e9ef4774942ff8f19812602768c6077981a
Perry Janssen <perry@burst-digital.com>
2020-01-24 20:18:16 +0100

    disable seeds and migrate safe to keep data

--- config/seeds.js
+++ config/seeds.js
@@ -9,27 +9,27 @@
  */
 
 module.exports.seeds = {
-  user: [
-    {
-      name: 'John Wayne',
-      email: 'johnnie86@gmail.com',
-      avatar: 'https://randomuser.me/api/portraits/men/83.jpg',
-      location: 'Mombasa',
-      bio: 'Spends most of my time at the beach'
-    },
-    {
-      name: 'Peter Quinn',
-      email: 'peter.quinn@live.com',
-      avatar: 'https://randomuser.me/api/portraits/men/32.jpg',
-      location: 'Langley',
-      bio: 'Rather not say'
-    },
-    {
-      name: 'Jane Eyre',
-      email: 'jane@hotmail.com',
-      avatar: 'https://randomuser.me/api/portraits/women/94.jpg',
-      location: 'London',
-      bio: 'Loves reading motivation books'
-    }
-  ],
+  // user: [
+  //   {
+  //     name: 'John Wayne',
+  //     email: 'johnnie86@gmail.com',
+  //     avatar: 'https://randomuser.me/api/portraits/men/83.jpg',
+  //     location: 'Mombasa',
+  //     bio: 'Spends most of my time at the beach'
+  //   },
+  //   {
+  //     name: 'Peter Quinn',
+  //     email: 'peter.quinn@live.com',
+  //     avatar: 'https://randomuser.me/api/portraits/men/32.jpg',
+  //     location: 'Langley',
+  //     bio: 'Rather not say'
+  //   },
+  //   {
+  //     name: 'Jane Eyre',
+  //     email: 'jane@hotmail.com',
+  //     avatar: 'https://randomuser.me/api/portraits/women/94.jpg',
+  //     location: 'London',
+  //     bio: 'Loves reading motivation books'
+  //   }
+  // ],
 };
